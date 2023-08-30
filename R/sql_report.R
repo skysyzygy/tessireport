0dca7665c097f6ed1fb750de083e99979b365a7f
skysyzygy <solson@bam.org>
2023-08-30 14:12:39 -0400

    Working sql_report with tests and documentation

--- R/sql_report.R
+++ R/sql_report.R
@@ -3,18 +3,51 @@
 #' Report that wraps the functionality of [tessilake::read_sql] and [send_xlsx]
 #'
 #' @param report sql_report object
-#' @param ... not used
+#' @param ... additional parameters passed on to [tessilake::read_sql] and [send_xlsx]
 #' @name sql_report
+#' @usage run(sql_report, query, ...)
+#' @note Useful additional parameters include:
+#' * `subject`: character subject of the email, default is `sql_report` and the current date.
+#' * `body`: character body of the email, default is a human readable message indicating the computer name.
+#' * `emails`: character email addresses to send the email to (first will be sender as well)
+#' * `dry_run`: boolean do not send the email if TRUE
+#' * `html`: boolean indicating whether the body of the email should be parsed as HTML. Default is `TRUE`.
+#' * `inline`: boolean indicating whether images in the HTML file should be embedded inline.
+#' * `file.names`: character name of the filename to show in the email. The default is `sql_report_<today's date>.xlsx`
+#' * `freshness`: difftime, the returned data will be at least this fresh.
 
 sql_report <- new_report(class="sql_report")
 
 #' @describeIn sql_report reads sql_report data
-#' Loads data from sql
-#' * p2: unsubscribes and hard bounces by list (uses p2_stream_enriched from tessistream)
-#' * tessi: emails, addresses, logins, memberships, constituencies, MGOs (attributes)
+#' @param query character sql query to generate the report
 #' @importFrom tessilake read_sql
+#' @importFrom checkmate expect_character
+#' @importFrom dplyr collect
+#' @importFrom utils modifyList
 #' @export
-read.sql_report <- function(query) {
+read.sql_report <- function(report, query, ...) {
   expect_character(query, len = 1)
 
+  # compute args from the formals of `read_sql` and `...` plus `query` and enforcing non-incremental loads
+  args <- modifyList(rlang::list2(...), list(query=query, incremental=FALSE)) %>%
+    .[intersect(names(.), rlang::fn_fmls_names(tessilake::read_sql))]
+
+  report$data <- do.call(read_sql,args) %>% collect
+
+  report
 }
+
+
+#' @describeIn sql_report sends sql_report data
+#'
+#' @importFrom tessilake read_sql
+#' @export
+output.sql_report <- function(report, query, ...) {
+  sql_report <- report$data
+
+  send_xlsx(table = sql_report, ...)
+
+  report
+}
+
+process.sql_report <- write.sql_report <- function(x,...) x
--- tests/testthat/test-sql_report.R
+++ tests/testthat/test-sql_report.R
@@ -1,3 +1,76 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+withr::local_package("checkmate")
+withr::local_package("mockery")
+
+# read.sql_report ---------------------------------------------------------
+
+test_that("read.sql_report queries the database", {
+  table <- data.table(a = runif(1000))
+  stub(read.sql_report, "read_sql", table)
+
+  report <- read(sql_report,"select * from test_table")
+
+  expect_length(report,1)
+  expect_equal(report$data, table)
 })
+
+test_that("read.sql_report passes parameters on to read_sql", {
+  table <- data.table(a = runif(1000))
+  read_sql <- mock(table)
+  stub(read.sql_report, "read_sql", read_sql)
+
+  report <- read(sql_report,"select * from test_table", freshness = -999)
+
+  expect_length(mock_args(read_sql),1)
+  expect_equal(mock_args(read_sql)[[1]][["freshness"]],-999)
+})
+
+# output.sql_report -------------------------------------------------------
+
+test_that("output.sql_report sends an excel file", {
+  sql_report$data <- data.table(a = runif(1000))
+  send_xlsx <- mock()
+  stub(output.sql_report, "send_xlsx", send_xlsx)
+
+  output(sql_report)
+
+  expect_length(mock_args(send_xlsx),1)
+  expect_equal(mock_args(send_xlsx)[[1]][["table"]], sql_report$data)
+})
+
+test_that("output.sql_report passes on additional parameters to send_xlsx", {
+  sql_report$data <- data.table(a = runif(1000))
+  send_xlsx <- mock()
+  stub(output.sql_report, "send_xlsx", send_xlsx)
+
+  output(sql_report, subject = "subject", body = "body")
+
+  expect_length(mock_args(send_xlsx),1)
+  expect_equal(mock_args(send_xlsx)[[1]][["table"]], sql_report$data)
+  expect_equal(mock_args(send_xlsx)[[1]][["subject"]], "subject")
+  expect_equal(mock_args(send_xlsx)[[1]][["body"]], "body")
+})
+
+# run.sql_report ----------------------------------------------------------
+
+test_that("run.sql_report passes on parameters", {
+  table <- data.table(a = runif(1000))
+  read_sql <- mock(table)
+  send_xlsx <- mock()
+  stub(read.sql_report, "read_sql", read_sql)
+  stub(output.sql_report, "send_xlsx", send_xlsx)
+  stub(run.report, "read.sql_report", read.sql_report)
+  stub(run.report, "output.sql_report", output.sql_report)
+
+  run(sql_report, "select * from test_table", freshness = -999, subject = "subject", body = "body")
+
+  expect_length(mock_args(read_sql),1)
+  expect_equal(mock_args(read_sql)[[1]][["query"]],"select * from test_table")
+  expect_equal(mock_args(read_sql)[[1]][["freshness"]],-999)
+  expect_in(names(mock_args(read_sql)[[1]]), rlang::fn_fmls_names(tessilake::read_sql))
+
+  expect_length(mock_args(send_xlsx),1)
+  expect_equal(mock_args(send_xlsx)[[1]][["table"]], table)
+  expect_equal(mock_args(send_xlsx)[[1]][["subject"]], "subject")
+  expect_equal(mock_args(send_xlsx)[[1]][["body"]], "body")
+})
+
