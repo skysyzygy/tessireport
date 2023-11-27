dc0fc49d31d0528b180e3f00ec4bc00b3266fbfe
skysyzygy <solson@bam.org>
2023-11-26 22:38:01 -0500

    Test stubs for duplicates_report

---
+++ tests/testthat/test-duplicates_report.R
@@ -0,0 +1,14 @@
+test_that("read.duplicates_report loads the duplicates stream", {
+  expect_equal(2 * 2, 4)
+})
+
+test_that("output.duplicates_report segments duplicates by customer number", {
+  expect_equal(2 * 2, 4)
+})
+
+test_that("output.duplicates_report reorders and renames the columns", {
+  expect_equal(2 * 2, 4)
+})
+test_that("output.duplicates_report dispatches to send_xlsx", {
+  expect_equal(2 * 2, 4)
+})
