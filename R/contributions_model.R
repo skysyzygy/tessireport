918be51fb1debada97316082056a34433c74e6d4
Sky Syzygy <ssyzygy@bam.org>
2024-06-20 18:09:16 -0400

    Building out mlr3 ingest using duckdb/dplyr/arrow out-of-memory table + converting this table from ffdf

--- R/contributions_model.R
+++ R/contributions_model.R
@@ -6,19 +6,43 @@ contributions_model <- report(class=c("contributions_model","mlr_report"))
 
 
 #' @export
-read.contributions_model <- function(contributions_model) {
-  stream_path <- file.path(tessilake::cache_path("","deep",".."),"stream","addressStream.gz")
+read.contributions_model <- function(model) {
+  stream_path <- file.path(tessilake::cache_path("","deep",".."),"stream","stream-20240619.gz")
   ffbase::unpack.ffdf(stream_path)
 
   positive_rows <- which(ff::as.ram(stream$event_type) == "Contribution")
   negative_rows <- setdiff(seq(nrow(stream)),positive_rows) %>% sample(length(.)/10)
   recent_rows <- which(ff::as.ram(stream$timestamp) >= (Sys.Date() - 365))
+  # --- censor
 
-  rows <- unique(sort(c(positive_rows, negative_rows, recent_rows)))
+  all_rows <- data.table::data.table(I = unique(sort(c(positive_rows, negative_rows, recent_rows))))
+  all_rows[,partition := sort(rep(1:10,length.out=.N))]
 
-  dataset <- stream[rows,]
+  for (rows in split(all_rows,by="partition")) {
+    dataset <- stream[rows$I,]
+    dataset <- cbind(dataset,rows)
+    # -- add event signal + rollback
+    tessilake::write_cache(dataset, "contributions", "model", partition = "partition",
+                overwrite = TRUE, sync = FALSE)
+    # ---
 
-  write_cache(dataset,"acquisition_stream","stream")
+  }
+
+  ff::delete(stream)
+  dataset <- tessilake::read_cache("contributions","model")
+
+  # --- refactor as function
+  db <- DBI::dbConnect(duckdb::duckdb())
+  duckdb::duckdb_register_arrow(db, "dataset", dataset)
+  table <- dplyr::tbl(db, "dataset")
+  backend <- mlr3db::DataBackendDplyr$new(table, "I", strings_as_factors = FALSE)
+  # ---
+
+  model$task <- mlr3proba::as_task_surv(backend, time = "timestamp", event = "event",
+                          type = "right", id = "contributions")
+
+  # --- label rows + columns
 
   NextMethod()
 }
+
