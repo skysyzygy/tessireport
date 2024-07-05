57724771e01e10c42001b0ce9817c0cfcc65ee3c
Sky Syzygy <ssyzygy@bam.org>
2024-07-05 16:25:07 -0400

    Working first contributions model with documentation!

--- R/contributions_model.R
+++ R/contributions_model.R
@@ -1,12 +1,17 @@
 #' @title contributions_model
-#' @description mlr3 model for predicting contributions
+#' @description mlr3 model for predicting a customer's first contribution
 #' @export
 #' @name contributions_model
 contributions_model <- report(class=c("contributions_model","mlr_report"))
 
-
-#' @export
-read.contributions_model <- function(model, n_partitions = 10) {
+#' @describeIn contributions_model Build the contributions dataset from the overall stream.
+#' * events are the first contribution per household > $50
+#' * data after an event are censored
+#' * contribution indicators are rolled back and timestamps are normalized to the start of customer activity
+#' * only data since `since` are loaded
+#' Data is written to the primary cache partitioned by year and then synced across storages
+#' @param since Date/POSIXct data on or after this date will be loaded and possibly used for training
+contributions_dataset <- function(since = Sys.Date()-365*5, ...) {
   stream_path <- file.path(tessilake::cache_path("","deep",".."),"stream","stream-20240619.gz")
   ffbase::unpack.ffdf(stream_path)
 
@@ -16,53 +21,149 @@ read.contributions_model <- function(model, n_partitions = 10) {
 
   # add event
   stream_key[,event := event_type == "Contribution" & contributionAmt>=50]
-  stream_key[,n_event := cumsum(event), by="group_customer_no"]
+  stream_key[,`:=`(n_event = cumsum(event),
+                   N = .N), by="group_customer_no"]
   # censor
-  stream_key <- stream_key[n_event == 0 | event & n_event==1]
-  stream_key[,n_event := NULL]
-  # subset
-  stream_key[,weight := 1/pmax(timestamp >= Sys.Date()-365 | event, .1)]
-  stream_key <- stream_key[runif(.N) < 1/weight]
+  stream_key <- stream_key[n_event == 0 | event & n_event==1 & N>1]
+  stream_key[,`:=`(n_event = NULL, N = NULL)]
+  # subsample
+  month_subset <- stream_key[,last(I), by=list(group_customer_no,lubridate::floor_date(timestamp,"months"))]$V1
+  stream_key <- stream_key[event | I %in% month_subset]
+  stream_key <- stream_key[timestamp >= since]
 
+  # partition by year
+  stream_key[,partition := year(timestamp)]
 
-  # partition by group
-  groups <- stream_key[,.(group_customer_no = unique(group_customer_no))]
-  groups[,partition := rep(seq_len(n_partitions), length.out = .N)]
-  stream_key <- merge(stream_key,groups, by="group_customer_no")
-
-  for (rows in split(stream_key,by="partition")) {
+  stream_chunk_write <- \(rows, partition) {
     dataset <- stream[rows$I,]
-    dataset <- cbind(dataset,rows[,.(I,event,partition,weight)])
     setDT(dataset)
+
+    dataset <- cbind(dataset,rows[,setdiff(colnames(rows),
+                                           colnames(dataset)), with = F])
 
     # normalize names for mlr3
     setnames(dataset, names(dataset), \(.) gsub("\\W","_",.))
 
     stream_rollback_event(dataset, columns = grep("^contribution",names(dataset),value = T))
+
+    dataset[,date := timestamp]
     stream_normalize_timestamps(dataset)
-    tessilake::write_cache(dataset, "contributions", "model", partition = "partition",
-                overwrite = TRUE, sync = FALSE)
+
+    dataset[,partition := partition]
+
+    tessilake::write_cache(dataset, "contributions_dataset", "model", partition = "partition",
+                           incremental = TRUE, date_column = "date", sync = FALSE)
 
   }
 
-  #ff::delete(stream)
-  dataset <- tessilake::read_cache("contributions","model")
+  stream_key[, stream_chunk_write(.SD,partition), by = "partition"]
 
-  model$task <- mlr3::Task$new(id = "contributions",
-                               task_type = "surv",
-                               backend = arrow_to_mlr3(dataset))
+  ff::delete(stream)
+}
+
+#' @export
+#' @importFrom tessilake read_cache cache_exists_any
+#' @importFrom dplyr filter select collect mutate
+#' @importFrom mlr3 TaskClassif
+#' @describeIn contributions_model Read in contribution data and prepare a MLR3 task
+#' @param model `contributions_model` object
+#' @param predict_since Date/POSIXct data on/after this date will be used to make predictions and not for training
+#' @param until Date/POSIXct data after this date will not be used for training or predictions, defaults to the beginning of today
+#' @param rebuild_dataset boolean rebuild the dataset by calling `contributions_dataset` (TRUE) or just read the existing one (FALSE)
+#' @note Data will be loaded in-memory, because *[inaudible]* mlr3 doesn't work well with factors encoded as dictionaries in arrow tables.
+read.contributions_model <- function(model, rebuild_dataset = FALSE,
+                                     since = Sys.Date()-365*5,
+                                     until = Sys.Date(),
+                                     predict_since = Sys.Date() - 30, ...) {
+
+  if(rebuild_dataset | !cache_exists_any("contributions_dataset","model")) contributions_dataset(since = since)
+
+  dataset <- read_cache("contributions_dataset","model") %>%
+    filter(date >= since & date < until) %>% collect
+
+  model$task <- TaskClassif$new(id = "contributions",
+                                target = "event",
+                                backend = dataset)
 
   # label columns + rows
-  model$task$col_roles$target <- c("timestamp","event")
-  model$task$col_roles$feature <- grep("^(email|contribution|address|ticket).+(amt|count|level|max|min)",
+  model$task$col_roles$feature <- grep("^(email|contribution|address|ticket).+(amt|count|level|max|min)|timestamp",
                                        names(dataset),value=T,ignore.case=T,perl=T)
-  model$task$col_roles$stratum <- "group_customer_no"
-  model$task$col_roles$weight <- "weight"
-  model$task$col_roles$order <- "timestamp"
 
-  model$task$row_roles$test <- stream_key[timestamp >= Sys.Date()-365, I]
+  model$task$col_roles$order <- "timestamp"
+  model$task$col_roles$group <- "group_customer_no"
+  model$task$positive <- "TRUE"
+
+  model$task$divide(ids = dataset[date >= predict_since,.I])
 
   NextMethod()
+}
+
+#' @export
+#' @importFrom tessilake cache_primary_path
+#' @importFrom mlr3verse po to_tune flt lts ppl tune `%>>%` p_int tnr selector_invert selector_grep
+#' @importFrom mlr3 msr rsmp as_learner lrn
+#' @describeIn contributions_model Tune and train a stacked log-reg/ranger model on the data
+#' @details
+#' # Preprocessing:
+#' * ignore 1-day and email "send" features because they leak data
+#' * remove constant features
+#' * balance classes to a ratio of 1:10 T:F
+#' * Yeo-Johnson with tuned boundaries
+#' * impute missing values out-of-range and add missing data indicator features
+#' * feature importance filter (for log-reg submodel only)
+#' # Model:
+#' * stacked log-reg + ranger > log-reg model
+#' * tuned using a hyperband method on the AUC (sensitivity/specificity)
+train.contributions_model <- function(model, ...) {
+  subsample <- po("subsample", frac = .1)
+
+  preprocess <- po("select",selector = selector_invert(selector_grep("__1|Send"))) %>>%
+                po("removeconstants",abs_tol=.5) %>>%
+                po("classbalancing", reference = "minor",ratio = 10,adjust="downsample") %>>%
+                po("yeojohnson", lower = to_train(-2,0), upper = to_train(0,2), eps = .1) %>>%
+                list(po("missind"),
+                     po("imputeoor")) %>>%
+                po("featureunion") %>>%
+                po("removeconstants",id="post_removeconstants",ratio=1/100)
+
+  importance_filter <- po("filter",
+                   filter = flt("importance"),
+                   filter.frac = to_tune(0,.67))
+
+  logreg <- as_learner(importance_filter %>>% lrn("classif.log_reg", predict_type = "prob"), id = "logreg")
+  ranger <- as_learner(lrn("classif.ranger", predict_type = "prob",
+                           mtry.ratio = to_tune(0,1),
+                           sample.fraction = to_tune(.1,1),
+                           num.trees = to_tune(p_int(16,128,tags="budget"))), id = "ranger")
+
+  stacked <- as_learner(subsample %>>% preprocess %>>% ppl("stacking", c(logreg,ranger),
+                                                           lrn("classif.log_reg", predict_type = "prob"),
+                                                           use_features = FALSE), id = "stacked")
+
+  stacked_tuned <- tune(
+      tuner = tnr("hyperband",eta=2),
+      task = model$task,
+      learner = stacked,
+      resampling = rsmp("holdout"),
+      measure = msr("classif.auc"))
+
+  stacked$param_set$values <- stacked_tuned$result_learner_param_vals
+
+  model$model <- stacked$train(model$task)
+  saveRDS(model$model, cache_primary_path("contributions.model","model"))
+
+}
+
+#' @export
+#' @importFrom tessilake cache_primary_path read_cache
+#' @importFrom dplyr filter
+#' @describeIn contributions_model Predict using the trained model
+predict.contributions_model <- function(model, ...) {
+  if(is.null(model$model))
+    model$model <- readRDS(cache_primary_path("contributions.model","model"))
+
+  model$predictions <- model$model$predict(model$task$internal_valid_task)
+
 }
 
 #' arrow_to_mlr3
