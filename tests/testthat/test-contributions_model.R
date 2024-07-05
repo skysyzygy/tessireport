a59dd40f9ba7b1b3232430cddf8bd02e54a2c5d1
Sky Syzygy <ssyzygy@bam.org>
2024-07-05 16:27:37 -0400

    Basic tests for contributions_dataset and read.contributions_model

--- tests/testthat/test-contributions_model.R
+++ tests/testthat/test-contributions_model.R
@@ -1,6 +1,50 @@
 withr::local_package("checkmate")
 withr::local_package("mockery")
 
+
+# contributions_dataset ------------------------------------------------
+
+test_that("contributions_dataset reads from an ffdf and adds an event indicator", {
+  tessilake::local_cache_dirs()
+  stub(contributions_dataset, "ffbase::unpack.ffdf", function(...) {
+    assign("stream",data.table(
+      group_customer_no = 1,
+      event_type = c("Ticket","Contribution"),
+      contributionAmt = 50,
+      timestamp = Sys.time()),
+      envir = rlang::caller_env())
+  })
+  stub(contributions_dataset, "ff::delete", TRUE)
+  contributions_dataset()
+
+  dataset <- read_cache("contributions","model") %>% collect
+  expect_equal(nrow(dataset),1)
+  expect_equal(dataset[1,"event"][[1]],TRUE)
+
+})
+
+test_that("contributions_dataset censors and subsets", {
+})
+
+test_that("contributions_dataset partitions by group_customer_no and writes an Arrow Dataset", {
+})
+
+
+# read.contributions_model ------------------------------------------------
+
+
+test_that("read.contributions_model returns a mlr3 classification task", {
+  stub(read.contributions_model, "read_cache",
+       \(...) {arrow::read_parquet("test-contributions_model.parquet", as_data_frame = F)})
+
+  model <<- read(contributions_model)
+
+  expect_class(model$task, "TaskClassif")
+
+})
+
+
+
 # stream_rollback_event ---------------------------------------------------
 
 test_that("stream_rollback_event rolls back data matching `columns`", {
