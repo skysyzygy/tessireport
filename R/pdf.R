6d8bb8d0be9346e4b95c2aeffba1177bd698bfdb
Sky Syzygy <ssyzygy@bam.org>
2024-07-31 12:11:13 -0400

    Moved pdf_report to a more general write_pdf implementation that will work with any expression

---
+++ R/pdf.R
@@ -0,0 +1,66 @@
+
+
+
+#' pdf_plot
+#'
+#' Add a plot to a pdf by generating R markdown, intended to be used within the `expr` argument to [write_pdf]
+#'
+#' @param plot output from ggplot or other plot that can be `print`ed
+#' @param title character optional title of the plot
+#' @param subtitle character subtitle of the plot
+#'
+#' @export
+#' @examples
+#' library(ggplot2)
+#' p <- ggplot(data.frame(x=runif(100))) + geom_histogram(aes(x))
+#' write_pdf(pdf_plot(p, title = "Sample plot", subtitle = "histogram of random numbers"))
+#'
+pdf_plot <- function(plot, title = NULL, subtitle = NULL) {
+  cat("\n\\clearpage\n")
+  cat(paste("\n###",title,"  \n"))
+  cat(paste("\n",subtitle,"  \n"))
+  print(plot)
+}
+
+pdf_table <- knitr::kable
+
+#' write_pdf
+#'
+#' @param expr expression to execute to create the body of the pdf
+#' @param preamble filename of the yaml preamble used by pandoc for generating the pdf
+#' @param .title character title for the header of the pdf; see the preamble for how this variable is used
+#' @param .author character author for the header of the pdf; see the preamble for how this variable is used
+#' @param .date character date for the header of the pdf; see the preamble for how this variable is used
+#' @param .classoption character tex classoption; see the preamble for how this variable is used
+#' @param .papersize character tex pagesize; see the preamble for how this variable is used
+#' @param .mainfont character tex mainfont; see the preamble for how this variable is used
+#' @param .geometry character tex geometry; see the preamble for how this variable is used
+#' @param .fontsize character tex fontsize; see the preamble for how this variable is used
+#' @param fig.width character knitr figure width; see the preamble for how this variable is used
+#' @param fig.height character knitr figure height; see the preamble for how this variable is used
+#'
+#' @export
+#' @importFrom lubridate today
+#' @importFrom rlang enquo eval_tidy
+write_pdf <- function(expr, output_file = tempfile(fileext = ".pdf"),
+                      .title = NULL, .author = NULL, .date = today(),
+                      .classoption = "landscape", .papersize = "legal", .mainfont = "Arial", .geometry = "margin=0.5in", .fontsize = "8pt",
+                      fig.width = 14, fig.height = 7.5,
+                      preamble = system.file("pdf_memoir-preamble.Rmd", package = "tessireport")) {
+
+  expr = rlang::enquo(expr)
+
+  # read preamble thru knitr::knit(text) to embed variables
+  pre <- knitr::knit(text = readLines(preamble))
+
+  # write out call
+  rmd_file <- tempfile(fileext = ".Rmd")
+
+  setup <- quote(knitr::opts_chunk$set(echo = FALSE, cache = FALSE, message = FALSE, warning = FALSE, error = FALSE, fig.width = fig.width, fig.height = fig.height))
+
+  writeLines(c(pre,'``` {r setup, echo = F}',deparse(setup),'```','``` {r results="asis"}','eval_tidy(expr)','```'), rmd_file, sep = '\n')
+
+  rmarkdown::render(rmd_file, output_file = output_file, clean = TRUE)
+
+  output_file
+}
