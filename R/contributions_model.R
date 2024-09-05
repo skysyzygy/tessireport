e96b79237f4ebd98d51bf5e893cd9a5f7458fa08
Sky Syzygy <ssyzygy@bam.org>
2024-09-05 16:30:34 -0400

    Fix broken link

--- R/contributions_model.R
+++ R/contributions_model.R
@@ -198,7 +198,7 @@ predict.contributions_model <- function(model, ...) {
 #' @importFrom purrr walk
 #' @importFrom tessilake cache_primary_path
 #' @importFrom stats runif
-#' @param downsample [numeric(1)] the amount to downsample the test set by for feature importance and
+#' @param downsample `numeric(1)` the amount to downsample the test set by for feature importance and
 #' Shapley explanations
 #' @export
 output.contributions_model <- function(model, downsample = .01, ...) {
