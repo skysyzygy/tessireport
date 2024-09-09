062baa96b46a61cc8ef48d4916f4ebd6c2b418a0
Sky Syzygy <ssyzygy@bam.org>
2024-09-09 16:06:26 -0400

    Fix test-mlr_report to match new load/save model API

--- tests/testthat/test-mlr_report.R
+++ tests/testthat/test-mlr_report.R
@@ -10,12 +10,13 @@ test_that("save_model serializes a model to a cache dir and syncs it", {
 
   save_model(model, "test_model", sync = F)
 
-  expect_file_exists(cache_path("test_model/test.model","deep","model"))
-  expect_failure(expect_file_exists(cache_path("test_model/test.model","shallow","model")))
+  expect_file_exists(cache_path("model.Rds","deep","test_model"))
+  expect_failure(expect_file_exists(cache_path("test.model","shallow","test_model")))
 
+  dir.create(cache_path("","shallow","test_model"))
   save_model(model, "test_model", sync = T)
-  expect_file_exists(cache_path("test_model/test.model","shallow","model"))
-  expect_file_exists(cache_path("test_model/test.model","deep","model"))
+  expect_file_exists(cache_path("model.Rds","shallow","test_model"))
+  expect_file_exists(cache_path("model.Rds","deep","test_model"))
 
 })
 
